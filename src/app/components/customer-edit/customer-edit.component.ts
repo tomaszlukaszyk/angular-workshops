7e24570e55c8b7f6a1430c8ac724e1a41d2184dc
Tomasz Lukaszyk <tomasz.lukaszyk@avanade.com>
2020-05-13 14:00:58 +0200

    Added form to dialog for editing customer

--- src/app/components/customer-edit/customer-edit.component.ts
+++ src/app/components/customer-edit/customer-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-edit',
@@ -17,8 +17,22 @@ export class CustomerEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.customerForm = this.fb.group({
-      name: [this.data?.name ?? ''],
+      // name: [this.data?.name ?? ''],
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      phoneNumbers: new FormArray([])
     })
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  addPhoneNumber() {
+    const formArr = this.customerForm.controls.phoneNumbers as FormArray;
+    formArr.push(this.fb.group({
+      label: new FormControl(''),
+      number: new FormControl()
+    }));
+  }
 }
